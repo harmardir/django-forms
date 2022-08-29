c5dee21b75d84ab506ec73bbfa4ae3ce772fc95d
Harout Mardirossian <harmardir@users.noreply.github.com>
2022-08-29 13:58:44 +0300

    adding security for saving password

--- coffeeshop/signupform/forms.py
+++ coffeeshop/signupform/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 from .models import User
+from django.contrib.auth.hashers import make_password
 
 class NameForm(forms.Form):
     name = forms.CharField(label='Your name', max_length=100)
@@ -10,4 +11,13 @@ class NameForm(forms.Form):
 class UserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['username','password']
+        fields = ['username','password', 'email']
+    def save(self, commit=True, *args, **kwargs):
+        m = super().save(commit=False)
+        m.password = make_password(self.cleaned_data.get('password'))
+        m.username = self.cleaned_data.get('username').lower()
+
+        if commit:
+            m.save()
+        return m
+    
--- coffeeshop/signupform/models.py
+++ coffeeshop/signupform/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200, null=True)
 
     def __str__(self):
         return self.username
