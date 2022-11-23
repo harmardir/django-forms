01f89cac53d74ee4643fe8cc0108d44174240ca9
Harout Mardirossian <harmardir@users.noreply.github.com>
2022-11-23 08:58:10 +0200

    removing old project files

--- coffeeshop/signupform/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    username = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-    email = models.EmailField(max_length=200, null=True)
-
-    def __str__(self):
-        return self.username
--- coffeeshop/signupform/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.Home, name='home'),
-    path('signup', views.Signup, name='signup'),
-    path('thanks', views.Thanks, name='thanks')
-]
