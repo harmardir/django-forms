32d9d30175e32a5a6d94b2ff790e0f12faf119ba
Harout Mardirossian <harmardir@users.noreply.github.com>
2022-11-24 16:33:23 +0200

    creating form

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import ContactForm
 
 def contact(request):
-    return HttpResponse("Contact View")
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+
+            name = form.cleaned_data['name']
+            email = form.cleaned_data["email"]
+
+            print(name , email)
+
+            
+    form = ContactForm()
+    return render(request, 'form.html', {'form': form})
